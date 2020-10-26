d6b7bf186378f36a944d257644bbec082a4a4cf5
Vdeub-cloudinary <loic.vdb@cloudinary.com>
2020-10-26 15:22:29 +0000

    fix issue with script by reloading the script needed in each component

--- src/script_handling/script.service.ts
+++ src/script_handling/script.service.ts
@@ -25,32 +25,28 @@ load(...scripts: string[]) {
 
 loadScript(name: string) {
     return new Promise((resolve, reject) => {
-        //resolve if already loaded
-        if (this.scripts[name].loaded) {
-            resolve({script: name, loaded: true, status: 'Already Loaded'});
-        }
-        else {
-            //load script
-            let script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = this.scripts[name].src;
-            if (script.readyState) {  //IE
-                script.onreadystatechange = () => {
-                    if (script.readyState === "loaded" || script.readyState === "complete") {
-                        script.onreadystatechange = null;
-                        this.scripts[name].loaded = true;
-                        resolve({script: name, loaded: true, status: 'Loaded'});
-                    }
-                };
-            } else {  //Others
-                script.onload = () => {
+
+        //load script
+        let script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = this.scripts[name].src;
+        if (script.readyState) {  //IE
+            script.onreadystatechange = () => {
+                if (script.readyState === "loaded" || script.readyState === "complete") {
+                    script.onreadystatechange = null;
                     this.scripts[name].loaded = true;
                     resolve({script: name, loaded: true, status: 'Loaded'});
-                };
-            }
-            script.onerror = (error: any) => resolve({script: name, loaded: false, status: 'Loaded'});
-            document.getElementsByTagName('head')[0].appendChild(script);
+                }
+            };
+        } else {  //Others
+            script.onload = () => {
+                this.scripts[name].loaded = true;
+                resolve({script: name, loaded: true, status: 'Loaded'});
+            };
         }
+        script.onerror = (error: any) => resolve({script: name, loaded: false, status: 'Loaded'});
+        document.getElementsByTagName('head')[0].appendChild(script);
+        
     });
 }
 
